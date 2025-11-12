744f30065ebe29ee710d9ac0771fa3aeda0cab7c
ekram-haque <dev.ekramul@gmail.com>
2025-11-13 02:07:33 +0600

    make crud api

--- index.js
+++ index.js
@@ -1,16 +1,77 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) =>{
-    res.send('pawmart server is running ');
-})
+const uri =
+  "mongodb+srv://pawmartdb:YZkA1sMkzpDrbGic@cluster0.gab2mh0.mongodb.net/?appName=Cluster0";
 
-app.listen(port, () =>{
-    console.log(`pawmart server is running on port ${port}`)
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("pawmart server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("pawmart_db");
+    const productsCollection = db.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find().sort({date: -1});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      const result = await productsCollection.insertOne(newProducts);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+        },
+      };
+
+      const result = await productsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log(" MongoDB connected successfully!");
+  } catch (error) {
+    console.error(" MongoDB connection error:", error);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(` pawmart server is running on port ${port}`);
+});
